b546f91435a5c2bfcba381f224ae798628366860
胥恒 <2289369299@qq.com>
2018-01-27 17:38:51 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var path = require("path");
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var session = require("express-session");
+var passport = require("passport");
+var flash = require("connect-flash");
+
+var routes = require("./routes");
+var setUpPassport = require("./setuppassport");
+
+var app = express();
+
+mongoose.connect("mongodb://localhost:27017/profile", function(err, data) {
+    console.log("connected to mongodb");
+});
+
+setUpPassport();
+
+app.set("port", process.env.PORT || 3000);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+app.use(session({
+  secret: "TKRv0IJs=HYqrvagQ#&!F!%V]Ww/4KiVs$s,<<MX",
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(routes);
+
+app.listen(app.get("port"), function() {
+  console.log("Server started on port " + app.get("port"));
+});
